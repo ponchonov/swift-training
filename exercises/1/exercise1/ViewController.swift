8c77629e57aa2c1502a5ebd68245984216ad8eed
Héctor Cuevas Morfín <poncho@Hectors-MacBook-Pro-2.local>
2016-06-01 15:04:37 -0500

    code fixed

--- exercises/1/exercise1/ViewController.swift
+++ exercises/1/exercise1/ViewController.swift
@@ -5,144 +5,70 @@
 //  Created by Héctor Cuevas Morfín on 5/31/16.
 //  Copyright © 2016 Swanros. All rights reserved.
 //
-
 import UIKit
 
+private extension Selector {
+    static let buttonTaped = #selector(ViewController.buttonClicked(_:))
+}
 class ViewController: UIViewController {
-
+    private lazy var myImageView: UIImageView = {
+        let i = UIImageView()
+        i.translatesAutoresizingMaskIntoConstraints = false
+        i.image = UIImage(named: "lemons.jpg")
+        i.contentMode = .ScaleAspectFill
+        return i
+    }()
+    private lazy var buttonLemon: UIButton = {
+        let i = UIButton()
+        i.setImage(UIImage(named: "lemon"), forState: .Normal)
+        i.translatesAutoresizingMaskIntoConstraints = false
+        i.addTarget(self, action: .buttonTaped, forControlEvents: UIControlEvents.TouchUpInside)
+        return i
+    }()
+    
+    private lazy var buttonBuild: UIButton = {
+        let i = UIButton()
+        i.setImage(UIImage(named: "build"), forState: .Normal)
+        i.translatesAutoresizingMaskIntoConstraints = false
+        i.addTarget(self, action: .buttonTaped, forControlEvents: UIControlEvents.TouchUpInside)
+        return i
+    }()
     let imageViewToDisplay = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.whiteColor()
-        doCreateView()
-        imageViewToDisplay.image = UIImage(named: "lemons.jpg")
-        imageViewToDisplay.contentMode = .ScaleAspectFill
-        imageViewToDisplay.translatesAutoresizingMaskIntoConstraints = false
-        //imageViewToDisplay.frame = CGRectMake(0, 0, 100, 100)
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    func doCreateView()  {
-        self.view.addSubview(imageViewToDisplay)
-        let buttonLemon = UIButton()
-        buttonLemon.tag = 1
-        let buttonBuild = UIButton()
-        buttonBuild.tag = 2
-        buttonLemon.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-        buttonBuild.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-        buttonLemon.setImage(UIImage(named: "lemon"), forState: .Normal)
-        buttonBuild.setImage(UIImage(named: "build"), forState: .Normal)
-        buttonLemon.translatesAutoresizingMaskIntoConstraints = false
-        buttonBuild.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(myImageView)
         self.view .addSubview(buttonBuild)
         self.view .addSubview(buttonLemon)
-        
-        let widthConsLemon = NSLayoutConstraint(
-            item: buttonLemon,
-            attribute: NSLayoutAttribute.Width,
-            relatedBy: NSLayoutRelation.Equal,
-            toItem: nil,
-            attribute: NSLayoutAttribute.NotAnAttribute,
-            multiplier: 1,
-            constant: 40)
+        doMakeConstraints()
 
-        let bottomConsLemon = NSLayoutConstraint(
-            item: buttonLemon,
-            attribute: .Bottom,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Bottom,
-            multiplier: 1, constant: -30)
-        
-        let leftConsLemon = NSLayoutConstraint(
-            item: buttonLemon,
-            attribute: .Left,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Left,
-            multiplier: 1, constant:  30);
-        
-       let widthConsBuild = NSLayoutConstraint(
-            item: buttonBuild,
-            attribute: NSLayoutAttribute.Width,
-            relatedBy: NSLayoutRelation.Equal,
-            toItem: nil,
-            attribute: NSLayoutAttribute.NotAnAttribute,
-            multiplier: 1,
-            constant: 40)
+    }
 
-        let bottomConsBuild = NSLayoutConstraint(
-            item: buttonBuild,
-            attribute: .Bottom,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Bottom,
-            multiplier: 1, constant: -30)
-        
-        
-        let rightConsBuild = NSLayoutConstraint(
-            item: buttonBuild,
-            attribute: .Right,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Right,
-            multiplier: 1, constant: -30);
-        
-        
-        let widthConsImage = NSLayoutConstraint(
-            item: imageViewToDisplay,
-            attribute: NSLayoutAttribute.Width,
-            relatedBy: .Equal,
-            toItem: nil,
-            attribute: .NotAnAttribute,
-            multiplier: 1,
-            constant: self.view.frame.size.width)
-        
-        let bottomConsImage = NSLayoutConstraint(
-            item: imageViewToDisplay,
-            attribute: NSLayoutAttribute.Bottom,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Bottom,
-            multiplier: 1,
-            constant: -100)
-        
-        let topConsImage = NSLayoutConstraint(
-            item: imageViewToDisplay,
-            attribute: .Top,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Top,
-            multiplier: 1, constant: 0)
-        
-        let leftConsImage = NSLayoutConstraint(
-            item: imageViewToDisplay,
-            attribute: .Left,
-            relatedBy: .Equal,
-            toItem: view,
-            attribute: .Left,
-            multiplier: 1, constant:  0);
-
-        self.view.addConstraints([leftConsImage,topConsImage,widthConsImage,bottomConsImage, widthConsBuild,bottomConsBuild,rightConsBuild,bottomConsLemon, leftConsLemon,widthConsLemon])
+    func doMakeConstraints()  {
+        let margins = view.layoutMarginsGuide
+        buttonLemon.widthAnchor.constraintEqualToConstant(40.0).active = true
+        buttonLemon.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor, constant: -30).active = true
+        buttonLemon.leftAnchor.constraintEqualToAnchor(margins.leftAnchor,constant: 30).active = true
+        buttonBuild.widthAnchor.constraintEqualToConstant(40.0).active = true
+        buttonBuild.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor, constant: -30).active = true
+        buttonBuild.rightAnchor.constraintEqualToAnchor(margins.rightAnchor, constant: -30).active = true
+        myImageView.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor,constant: 0).active = true
+        myImageView.topAnchor.constraintEqualToAnchor(margins.topAnchor,constant: 0).active = true
+        myImageView.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor,constant: 0).active = true
+        myImageView.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor,constant: -100).active = true
     }
 
     func buttonClicked(button: UIButton){
-        
-        if button.tag == 2
+        if button == buttonBuild
         {
-            imageViewToDisplay.image = UIImage(named: "builds.jpg")
+            myImageView.image = UIImage(named: "builds.jpg")
         }else
         {
-            imageViewToDisplay.image = UIImage(named: "lemons.jpg")
+            myImageView.image = UIImage(named: "lemons.jpg")
         }
     }
 
 }
+
 
